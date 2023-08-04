e3da94dbc290aa5ac49557fe9538e2a1524ba2fc
LidzhievNaran <lidzhievnaran483@gmail.com>
2023-08-04 12:17:00 +0300

    load 1

--- scripts/script.js
+++
@@ -1,6 +0,0 @@
-window.onload=function()
-{
-    document.getElementById("im").onclick = func;
-    document.getElementById("im").onmouseout = doc;
-    document.getElementById("im").onmouseover=res;
-};
