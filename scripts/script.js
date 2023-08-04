cb45f10e6fba8875ec37cd050f3c9c60ce929883
LidzhievNaran <lidzhievnaran483@gmail.com>
2023-08-04 12:10:10 +0300

    registration of several handlers at once

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+window.onload=function()
+{
+    document.getElementById("im").onclick = func;
+    document.getElementById("im").onmouseout = doc;
+    document.getElementById("im").onmouseover=res;
+};
